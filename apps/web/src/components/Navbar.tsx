a01aef0ec69b921791953c452334c7b9f984af70
Ridhwansan <82388761+Ridhwansan@users.noreply.github.com>
2024-08-07 16:01:15 +0700

    feat: add login and authguard (#3)

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,15 +1,33 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  //dalam redux, useAppSelector digunakan untuk membaca data
+  const { name, id } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
   return (
     <nav className="sticky top-0 bg-transparent">
       <div className="container mx-auto px-4">
         <div className="item-center flex justify-between py-2">
-          <h1 className="text-lg text-red-400 font-bold">Logo</h1>
+          <h1 className="text-lg font-bold text-red-400">Logo</h1>
+
           <div className="flex cursor-pointer items-center gap-8 font-medium">
             <h3>Home</h3>
             <h3>Write</h3>
             <h3>Profile</h3>
+            {id ? (
+              <>
+                <Link href="/profile">{name}</Link>
+                <h3 onClick={() => dispatch(logoutAction())}>Logout</h3>
+              </>
+            ) : (
+              <Link href="/login">Login</Link>
+            )}
           </div>
         </div>
       </div>
--- apps/web/src/hooks/api/auth/useLogin.tsx
+++ apps/web/src/hooks/api/auth/useLogin.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { axiosInstance } from "@/lib/axios";
+import useAxios from "@/hooks/useAxios";
+import { useAppDispatch } from "@/redux/hooks";
+import { loginAction } from "@/redux/slices/userSlice";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
@@ -12,18 +15,27 @@ interface LoginPayload {
 
 const useLogin = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const { axiosInstance } = useAxios();
+
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
   const login = async (payload: LoginPayload) => {
     setIsLoading(true);
     try {
+      const { data } = await axiosInstance.post("/api/auth/login", payload);
+      dispatch(loginAction(data));
       // /api/auth itu dari app
       // /login itu dari router
       await axiosInstance.post("/api/auth/login", payload);
       toast.success("login success");
+
+      //ini untuk ketika sudah login dia akan masuk ke halaman localhost3000/ 
+      router.push("/");
     } catch (error) {
-        if(error instanceof AxiosError) {
-            toast.error(error.response?.data)
-        }
-      
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data);
+      }
     } finally {
       setIsLoading(false);
     }
