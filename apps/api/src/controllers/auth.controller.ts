cdfcf7c270a031211c2b142cb5abaa002ebfc0d0
unknown <ridhsant@gmail.com>
2024-08-07 11:38:01 +0700

    feat: login

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
-import { NextFunction, Request, Response } from 'express';
-import prisma from '@/prisma';
+import { loginService } from '@/services/auth/login.service';
 import { registerService } from '@/services/auth/register.service';
+import { NextFunction, Request, Response } from 'express';
+
+//controller ini untuk menangani HTTP request dari client dan mereturnnya
 
 export class AuthController {
   async register(req: Request, res: Response, next: NextFunction) {
@@ -11,6 +13,17 @@ export class AuthController {
       next(error);
     }
   }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await loginService(req.body);
+      return res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
+
+
 
 console.log('Hello');
--- apps/web/src/features/login/index.tsx
+++ apps/web/src/features/login/index.tsx
@@ -8,50 +8,31 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFormik } from "formik";
-import { RegisterSchema } from "./schemas/RegisterSchema";
-import useRegister from "@/hooks/api/auth/useRegister";
+import { LoginSchema } from "./schemas/loginSchema";
+import useLogin from "@/hooks/api/auth/useLogin";
 
-const RegisterPage = () => {
-  const { register, isLoading } = useRegister();
+const LoginPage = () => {
+  const { login, isLoading } = useLogin();
 
   const formik = useFormik({
     initialValues: {
-      name: "",
       email: "",
       password: "",
     },
-    validationSchema: RegisterSchema,
-    //apa yang ditulis di dalam form akan masuk ke on submit
+    validationSchema: LoginSchema,
     onSubmit: async (values) => {
-      await register(values);
+      await login(values);
     },
   });
-
   return (
     <main className="flex justify-center pt-20">
       <Card className="w-[350px]">
         <CardHeader>
-          <CardTitle>Sign Up</CardTitle>
+          <CardTitle>Sign In</CardTitle>
         </CardHeader>
         <CardContent>
           <form onSubmit={formik.handleSubmit}>
             <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input
-                  name="name"
-                  type="text"
-                  value={formik.values.name}
-                  placeholder="Your name"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {!!formik.touched.name && !!formik.errors.name ? (
-                  <p className="text-xs text-red-500">{formik.errors.name}</p>
-                ) : (
-                  ""
-                )}
-              </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
                 <Input
@@ -97,4 +78,4 @@ const RegisterPage = () => {
   );
 };
 
-export default RegisterPage;
+export default LoginPage;
